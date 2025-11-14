1b29c854cd1c53139fe37ce4acd0efdb6f9a6742
jeankb1991 <86066466+jeankb1991@users.noreply.github.com>
2025-11-13 22:47:16 -0300

    Allow all hosts in Django settings

--- inventario_django/settings.py
+++ inventario_django/settings.py
@@ -6,7 +6,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'dev-secret-key-change-me'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*",]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
